24128d48107f41ad7b2a4474033abc7022818185
Ricardo Leão <113477303+Ricardoleaom@users.noreply.github.com>
2025-08-07 23:00:52 +0000

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,46 @@ import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
 import { productTable } from "@/db/schema";
 
+import CategorySelector from "../components/common/category-selector";
+
 const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true,
     },
   });
+  const categories = await db.query.categoryTable.findMany({});
 
   return (
     <>
       <Header />
       <div className="space-y-6">
         <div className="px-5">
-        <Image
-          src="/banner01.png"
-          alt="leve uma vida com estilo"
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-full"
+          <Image
+            src="/banner01.png"
+            alt="leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
           />
         </div>
 
         <ProductList products={products} title="Mais vendidos" />
 
-       <div className="px-5">
-         <Image
-          src="/banner02.png"
-          alt="leve uma vida com estilo"
-          height={0}
-          width={0}
-          sizes="100vw"
-          className="h-auto w-full"
-        />
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
         </div>
       </div>
     </>
