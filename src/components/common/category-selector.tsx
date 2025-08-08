684481def35b74b083855cecbee9b5243a0d6ff9
Ricardo Leão <113477303+Ricardoleaom@users.noreply.github.com>
2025-08-08 16:42:50 +0000

    feat: add category products page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,4 +1,7 @@
-import { categoryTable } from "../../db/schema";
+import Link from "next/link";
+
+import { categoryTable } from "@/db/schema";
+
 import { Button } from "../ui/button";
 
 interface CategorySelectorProps {
@@ -7,7 +10,7 @@ interface CategorySelectorProps {
 
 const CategorySelector = ({ categories }: CategorySelectorProps) => {
   return (
-    <div className="rounded-3x1 bg-[#F4EFFF] p-6">
+    <div className="rounded-3xl bg-[#F4EFFF] p-6">
       <div className="grid grid-cols-2 gap-3">
         {categories.map((category) => (
           <Button
@@ -15,7 +18,7 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             variant="ghost"
             className="rounded-full bg-white text-xs font-semibold"
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}>{category.name}</Link>
           </Button>
         ))}
       </div>
